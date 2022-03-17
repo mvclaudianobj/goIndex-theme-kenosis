4df4230cba31f9db8aea90cf08923ba877565e2a
Marcos Vinícius Claudiano <mvclaudiano@hotmail.com>
2022-03-17 18:34:07 -0300

    Update app.js

--- dist/themes/light/app.js
+++ dist/themes/light/app.js
@@ -849,10 +849,10 @@ function file_video(path) {
     player_items += `<li class="mdui-divider"></li>
                    <li class="mdui-menu-item"><a id="copy-link" class="mdui-ripple">复制链接</a></li>`;
     const playBtn = `
-      <button class="mdui-btn mdui-ripple mdui-color-theme-accent" mdui-menu="{target:'#player-items'}">
+      <!--<button class="mdui-btn mdui-ripple mdui-color-theme-accent" mdui-menu="{target:'#player-items'}">
         <i class="mdui-icon material-icons">&#xe039;</i>外部播放器播放<i class="mdui-icon material-icons">&#xe5cf;</i>
       </button>
-      <ul class="mdui-menu" id="player-items">${player_items}</ul>`;
+      <ul class="mdui-menu" id="player-items">${player_items}</ul>-->`;
 
     var playerUI;
     var playerType;
