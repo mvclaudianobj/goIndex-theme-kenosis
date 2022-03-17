2ffe790f5a39a760ceaca572e00fdc3d49a6faf0
Marcos Vinícius Claudiano <mvclaudiano@hotmail.com>
2022-03-17 20:28:08 -0300

    Update app.js

--- dist/themes/light/app.js
+++ dist/themes/light/app.js
@@ -776,11 +776,6 @@ function file_code(path) {
 <div class="mdui-container">
 <pre id="editor" ></pre>
 </div>
-<!--<div class="mdui-textfield">
-  <label class="mdui-textfield-label">下载地址</label>
-  <input class="mdui-textfield-input" type="text" value="${href}"/>
-</div>
-<a href="${href}" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>-->
 
 <script src="https://cdn.staticfile.org/ace/1.4.7/ace.js"></script>
 <script src="https://cdn.staticfile.org/ace/1.4.7/ext-language_tools.js"></script>
@@ -913,17 +908,7 @@ function file_video(path) {
   <br>
   ${playerUI}
   <br>
-  <!-- 固定标签 -->
-  <!--<div class="mdui-textfield">
-    <label class="mdui-textfield-label">下载地址</label>
-    <input class="mdui-textfield-input" type="text" value="${url}"/>
-  </div>
-  <div class="mdui-textfield">
-    <label class="mdui-textfield-label">HTML 引用地址</label>
-    <textarea class="mdui-textfield-input"><video><source src="${url}" type="video/mp4"></video></textarea>
-  </div>-->
 </div>
-<a href="${url}" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
   `;
     $('#content').html(content);
     $('#copy-link').on('click', () => {
@@ -1100,17 +1085,7 @@ function file_audio(path) {
   <br>
   ` + plyrUI + `
   <br>
-  <!-- 固定标签 -->
-  <!--<div class="mdui-textfield">
-    <label class="mdui-textfield-label">下载地址</label>
-    <input class="mdui-textfield-input" type="text" value="${url}"/>
-  </div>
-  <div class="mdui-textfield">
-    <label class="mdui-textfield-label">HTML 引用地址</label>
-    <textarea class="mdui-textfield-input"><audio><source src="${url}"></audio></textarea>
-  </div>-->
 </div>
-<!--<a href="${url}" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>-->
   `;
     $('#content').html(content);
 
@@ -1208,21 +1183,7 @@ function file_image(path) {
       <img class="mdui-img-fluid" src="${url}"/>
     </div>
   <br>
-  <!--<div class="mdui-textfield">
-    <label class="mdui-textfield-label">下载地址</label>
-    <input class="mdui-textfield-input" type="text" value="${url}"/>
-  </div>
-  <div class="mdui-textfield">
-    <label class="mdui-textfield-label">HTML 引用地址</label>
-    <input class="mdui-textfield-input" type="text" value="<img src='${url}' />"/>
-  </div>
-        <div class="mdui-textfield">
-    <label class="mdui-textfield-label">Markdown 引用地址</label>
-    <input class="mdui-textfield-input" type="text" value="![](${url})"/>
-  </div>-->
-        <br>
 </div>
-<!--<a href="${url}" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>-->
     `;
     //my code
     $('#content').html(content);
