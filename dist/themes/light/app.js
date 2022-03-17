091b23f0d10f68e555200191cf74d485c9a8f8d5
Marcos Vinícius Claudiano <mvclaudiano@hotmail.com>
2022-03-17 18:23:17 -0300

    Update app.js

--- dist/themes/light/app.js
+++ dist/themes/light/app.js
@@ -923,7 +923,7 @@ function file_video(path) {
     <textarea class="mdui-textfield-input"><video><source src="${url}" type="video/mp4"></video></textarea>
   </div>
 </div>
-<a href="${url}" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
+<!--<a href="${url}" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>-->
   `;
     $('#content').html(content);
     $('#copy-link').on('click', () => {
